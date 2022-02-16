32c086d5bad16e80c717056a679e30d86e779721
Soda <popsodapants@gmail.com>
2022-02-16 14:48:34 -0600

    asf

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -21,7 +21,7 @@ namespace Blackjack
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
