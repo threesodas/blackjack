914f5cd00ea3641ab02cc28bea8fef730258f774
Soda <popsodapants@gmail.com>
2022-02-16 14:44:38 -0600

    f*rtntie

---
+++ Blackjack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Blackjack
+{
+    struct Cards
+    {
+        struct Black
+        {
+            public static string[] Club = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
+            public static string[] Spade = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
+        }
+        struct Red
+        {
+            public static string[] Diamond = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
+            public static string[] Heart = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
+        }
+
+        public static string[] Phenotype = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    }
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
